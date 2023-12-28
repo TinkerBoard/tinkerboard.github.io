778acf42dd68e86b7650d673066f2809b17387d9
Leslie Yu <leslie_yu@asus.com>
2023-12-28 17:34:13 +0800

    Enable showLastUpdateAuthor and showLastUpdateTime for docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,6 +46,8 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/TinkerBoard/tinkerboard.github.io/tree/main/',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
         },
         blog: {
           showReadingTime: true,
